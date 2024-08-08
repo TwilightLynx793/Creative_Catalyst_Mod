0868b082adcbcaf50518d3e5a14d6bb84e951737
AashirZaman <ashirqwertz123@gmail.com>
2024-08-08 13:20:42 +0600

    Update

---
+++ src/main/java/net/twilightlynx/creative_catalyst/datagen/ModBlockTagProvider.java
@@ -0,0 +1,35 @@
+package net.twilightlynx.creative_catalyst.datagen;
+
+import net.twilightlynx.creative_catalyst.CreativeCatalyst;
+import net.twilightlynx.creative_catalyst.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, CreativeCatalyst.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.CITRINE_BLOCK.get())
+                .add(ModBlocks.RAW_CITRINE_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.CITRINE_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.RAW_CITRINE_BLOCK.get());
+
+        /*tag(BlockTags.FENCES).add(ModBlocks.BLACK_OPAL_FENCE.get());
+        tag(BlockTags.FENCE_GATES).add(ModBlocks.BLACK_OPAL_FENCE_GATE.get());
+        tag(BlockTags.WALLS).add(ModBlocks.BLACK_OPAL_WALL.get());*/
+    }
+}
